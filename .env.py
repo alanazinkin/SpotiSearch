c2365bc1bf45f6e36b2f80076adcb53124f42b82
Alana Zinkin <112518260+alanazinkin@users.noreply.github.com>
2025-11-30 21:26:05 -0500

    Delete .env.py

--- .env.py
+++
@@ -1,5 +0,0 @@
-# Use the local URI for testing the redirect flow
-GEMINI_API_KEY="AIzaSyA0lk0ovNmex4idlIbHQoQ-EaVM-cnJwAw"
-SPOTIPY_CLIENT_ID="c40d1ff7175f462f8e429d3a8c1cc832"
-SPOTIPY_CLIENT_SECRET="aacf0edd034b4506999de7942c5b66a3"
-SPOTIPY_REDIRECT_URI="http://localhost:8501"
